2b18a6b748205f1c7117aa65215bd034b62ac10d
Robert Haken <haken@havit.cz>
2024-05-20 16:19:22 +0200

    Primary constructors

---
+++ CSharp12Demo/01_PrimaryConstructors.cs
@@ -0,0 +1,60 @@
+﻿namespace PrimaryConstructors;
+
+public class Person(string name, int age)
+{
+	public string Name { get; } = name;
+	public int Age { get; } = age;
+}
+
+public class Student(string name, int age, string school) : Person(name, age)
+{
+	public string School { get; } = school;
+}
+
+public class Downloader(HttpClient httpClient)
+{
+	public string Download(string url)
+	{
+		return httpClient.GetStringAsync(url).Result;
+	}
+}
+
+
+
+
+// ================================================================
+// See ILSpy
+
+public class ParameterOnly(decimal celsiusTemperature)
+{
+	private decimal _kelvinTemperature = celsiusTemperature + 273.15m;
+	public string Print() => $"Kelvin: {_kelvinTemperature}";
+}
+
+public class ClassWithCapture(int id)
+{
+	public void DoSomething()
+	{
+		Console.WriteLine($"ID: {id}");
+	}
+
+	public void Mutate(int newId)
+	{
+		id = newId;
+	}
+}
+
+
+public class DoubleCapture(string name)
+{
+	public string Name { get; } = name; // 1. stored in a property
+
+	public int GetNameLength()
+	{
+		return name.Length; // 2. captured in a "<name>P" field
+	}
+
+}
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-12#primary-constructors
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/primary-constructors
