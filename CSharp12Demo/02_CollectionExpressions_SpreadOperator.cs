cb39be2841ca70e83b353a55f5fc412d36e8c49f
Robert Haken <haken@havit.cz>
2024-05-20 16:47:46 +0200

    Collection initializers

---
+++ CSharp12Demo/02_CollectionExpressions_SpreadOperator.cs
@@ -0,0 +1,46 @@
+﻿namespace CollectionExpressions;
+
+public class Demo
+{
+	public void Run()
+	{
+		// Create an array:
+		int[] a = [1, 2, 3, 4, 5, 6, 7, 8];
+
+		// Create a span
+		Span<int> b = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i'];
+
+		// Create a jagged 2D array:
+		int[][] twoD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+		// Create a jagged 2D array from variables:
+		int[] row0 = [1, 2, 3];
+		int[] row1 = [4, 5, 6];
+		int[] row2 = [7, 8, 9];
+		int[][] twoDFromVariables = [row0, row1, row2];
+
+		// spread operator
+		int[] c = [0, .. row0, .. row1, -9, -10]; // [0, 1, 2, 3, 4, 5, 6, -9, -10]
+
+		// target-typed compiler optimization
+		DoSomething([1, 2, 3]);	 
+		DoSomething2([1, 2, 3]);
+		DoSomething3([1, 2, 3]);	 
+	}
+
+	public void DoSomething(List<int> items)
+	{
+	}
+	
+	public void DoSomething2(IEnumerable<int> items)
+	{
+	}
+
+	public void DoSomething3(ReadOnlySpan<byte> bytes)
+	{
+	}
+}
+
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-12#collection-expressions
+// https://devblogs.microsoft.com/dotnet/announcing-csharp-12/#collection-expressions
