72d273d0bc6610afa58876dc14c86a6559ade6cf
Robert Haken <haken@havit.cz>
2024-05-20 17:14:19 +0200

    Inline arrays

---
+++ CSharp12Demo/03_InlineArrays.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+namespace InlineArrays;
+
+[InlineArray(10)] // known size (compile-time)
+public struct MyBuffer10
+{
+	private int _element0; // Inline array struct must declare one and only one instance field.
+}
+
+
+public static class Demo
+{
+	public static void Run()
+	{
+		MyBuffer10 buffer = new MyBuffer10();
+		for (int i = 0; i < 10; i++) // no buffer.Length
+		{
+			buffer[i] = i;
+		}
+
+		// can be exposed as a Span/ReadOnlySpan
+		var span = MemoryMarshal.CreateSpan(ref Unsafe.As<MyBuffer10, int>(ref buffer), 10);
+		DoSomething(span);
+	}
+
+	public static void DoSomething(Span<int> values)
+	{
+		foreach (var value in values)
+		{
+			Console.WriteLine(value);
+		}
+	}
+}
+
+
+public static class Demo2
+{
+	public static void Run()
+	{
+		Span<int> values = GetValues();
+		foreach (var value in values)
+		{
+			Console.WriteLine(value);
+		}
+	}
+
+	public static Span<int> GetValues()
+	{
+		MyBuffer10 buffer = new MyBuffer10();
+		for (int i = 0; i < 10; i++) // no buffer.Length
+		{
+			buffer[i] = i;
+		}
+
+		// can be exposed as a Span/ReadOnlySpan
+		return MemoryMarshal.CreateSpan(ref Unsafe.As<MyBuffer10, int>(ref buffer), 10);
+	}
+}
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-12.0/inline-arrays
+// Used in the runtime for performance-critical scenarios, e.g. collection expressions.
--- CSharp12Demo/Program.cs
+++ CSharp12Demo/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿InlineArrays.Demo.Run();
+//InlineArrays.Demo2.Run();
