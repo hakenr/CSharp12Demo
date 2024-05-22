67b73f707e8b0442c924cc56b0fce3a50231eb61
Robert Haken <haken@havit.cz>
2024-05-22 22:18:38 +0200

    Experimental attribute

---
+++ CSharp12Demo/07_ExperimentalAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalAttributeDemo
+{
+	[Experimental("HAKEN001")]
+	public class Foo
+	{
+	}
+
+	public class Demo
+	{
+		public void Run()
+		{
+#pragma warning disable HAKEN001
+			var f = new Foo();
+#pragma warning restore HAKEN001
+		}
+	}
+}
