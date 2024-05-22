6e7dd43cb913a97dc4d4a03fbe4e90b2f97de921
Robert Haken <haken@havit.cz>
2024-05-22 22:41:59 +0200

    Interceptors

---
+++ CSharp12Demo/08_Interceptors.cs
@@ -0,0 +1,3 @@
+﻿// EXPERIMENTAL FEATURE, PREVIEW in .NET 8 / C# 12
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-12#interceptors
+// https://github.com/dotnet/roslyn/blob/main/docs/features/interceptors.md
